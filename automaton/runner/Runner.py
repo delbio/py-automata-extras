b49e3644a72fb2f4a9a30ec8d33034f3ae6d0cc4
Fabio Del Bene <delbio87@gmail.com>
2018-06-19 00:42:08 +0200

    improved selection of action

--- automaton/runner/Runner.py
+++ automaton/runner/Runner.py
@@ -1,4 +1,25 @@
 
+def select_action_from_list(nextInputs):
+    if len(nextInputs) == 1:
+        return nextInputs[0]
+
+    actionName = input('User must select an action: ' + ', '.join(list(nextInputs)) + ' : ')
+    try:
+        i = int(actionName)
+        if 0 <= i < len(list(nextInputs)):
+            return nextInputs[i]
+        else:
+            print('\tSelect a number from 0 to ', len(list(nextInputs)) -1)
+            return select_action_from_list(nextInputs)
+    except ValueError as e:
+        pass
+
+    if actionName in nextInputs:
+        return actionName
+
+    return select_action_from_list(nextInputs)
+
+
 class SimpleRunner():
     def __init__(self):
         pass
@@ -8,11 +29,7 @@ class SimpleRunner():
         while not automaton.isFinished():
             nextInputs = automaton.getCurrentState().getNextInputs()
             print("\tNext inputs:\t" + ', '.join(list(nextInputs)))
-            actionName = None
-            if len(nextInputs) == 1:
-                actionName = nextInputs[0]
-            else:
-                actionName = input('User must select an action: ' + ', '.join(list(nextInputs)) + ' : ')
+            actionName = select_action_from_list(nextInputs)
             automaton.doAction(actionName)
             automaton.move(actionName)
             print('Current State:\t',automaton.getCurrentState())
--- example/Main.py
+++ example/Main.py
@@ -1,18 +1,11 @@
 import sys
 
-from automaton.core.Action import Action
-from automaton.core.Automaton import Automaton
+from defusedxml.ElementTree import parse
+
 from automaton.builder.XmlBuilder import XmlBuilder
 from automaton.runner.Runner import SimpleRunner
 
 sys.path.append('./actions')
-import actions.CopyFile
-import actions.GenerateTextFile
-import actions.CheckExit
-from States import CopiedFile, NotStarted, GeneratedTextFile, ExitChecked
-
-from defusedxml.ElementTree import fromstring, parse
-import xml.etree.ElementTree as ET
 
 
 def build_from_xml(filepath):
@@ -22,21 +15,8 @@ def build_from_xml(filepath):
     print('Loaded Automaton: \n', automaton.__str__())
     return automaton
 
-def runner(automaton):
-    print('Start from:\t', automaton.getCurrentState())
-    while not automaton.isFinished():
-        nextInputs = automaton.getCurrentState().getNextInputs()
-        print("\tNext inputs:\t" + ', '.join(list(nextInputs)))
-        actionName = None
-        if len(nextInputs) == 1:
-            actionName = nextInputs[0]
-        else:
-            actionName = input('User must select an action: ' + ', '.join(list(nextInputs)) + ' : ')
-        automaton.doAction(actionName)
-        automaton.move(actionName)
-        print('Current State:\t',automaton.getCurrentState())
 
 if __name__ == "__main__":
-   automaton =  build_from_xml('config.xml')
-   runner = SimpleRunner()
-   runner.run(automaton)
+    automaton =  build_from_xml('config.xml')
+    runner = SimpleRunner()
+    runner.run(automaton)
